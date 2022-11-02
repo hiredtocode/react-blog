258be189af06ec58cd50c5b7030d31242e0b6e0b
hiredtocode <jasonhan@kakao.com>
2022-11-02 14:11:56 +0900

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,21 @@
 const Home = () => {
-	const handleClick = () => {
-		console.log("Hello");
+	const handleClick = (event) => {
+		console.log("Hello", event);
 	};
 
-	const handleClickAgain = (name) => {
-		console.log("Hello " + name);
+	const handleClickAgain = (name, event) => {
+		console.log("Hello " + name, event.detail);
 	};
 
 	return (
 		<div className="home">
 			<h2>Homepage</h2>
 			<button onClick={handleClick}>Click me</button>
-			<button onClick={() => handleClickAgain("Jason")}>
+			<button
+				onClick={(event) =>
+					handleClickAgain("Jason", event)
+				}
+			>
 				Click again
 			</button>
 		</div>
