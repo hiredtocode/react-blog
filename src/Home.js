b9703986a253b10bdde55abb3d3267283aae7e5c
hiredtocode <jasonhan@kakao.com>
2022-11-02 21:07:09 +0900

    Added fake database server to pull data from

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-	const [blogs, setBlogs] = useState([
-		{
-			title: "My new website",
-			body: "lorem ipsum dolor sit amet",
-			author: "Mario",
-			id: 1,
-		},
-		{
-			title: "Welcome Party",
-			body: "lorem ipsum dolor sit amet",
-			author: "Yoshi",
-			id: 2,
-		},
-		{
-			title: "Web dev top tips!",
-			body: "lorem ipsum dolor sit amet",
-			author: "Mario",
-			id: 3,
-		},
-	]);
+	const [blogs, setBlogs] = useState(null);
 
 	const handleDelete = (id) => {
 		const newBlogs = blogs.filter((blog) => blog.id !== id);
 		setBlogs(newBlogs);
 	};
+
+	useEffect(() => {
+		fetch("http://localhost:8000/blogs")
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				setBlogs(data);
+			});
+	}, []);
 
 	return (
 		<div className="home">
