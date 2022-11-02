d3e2d0e0d0a854b6c00f67016ea6b706b79c99ad
hiredtocode <jasonhan@kakao.com>
2022-11-02 21:23:39 +0900

    Showing error messages to users if it occurs

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,30 @@ import BlogList from "./BlogList";
 const Home = () => {
 	const [blogs, setBlogs] = useState(null);
 	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
 		fetch("http://localhost:8000/blogs")
 			.then((response) => {
+				if (!response.ok) {
+					throw Error("Couldn't fetch the data");
+				}
 				return response.json();
 			})
 			.then((data) => {
 				setBlogs(data);
 				setIsLoading(false);
+				setError(null);
+			})
+			.catch((error) => {
+				setIsLoading(false);
+				setError(error.message);
 			});
 	}, []);
 
 	return (
 		<div className="home">
+			{error && <div>{error}</div>}
 			{isLoading && <div>Loading...</div>}
 			{blogs && (
 				<BlogList blogs={blogs} title="Awesome Blog!" />
